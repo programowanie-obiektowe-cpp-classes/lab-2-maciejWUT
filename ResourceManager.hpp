e1dda79eae252099f37cb61b7f5d93e3f947761c
maciejWUT <mciek.sieradzki@gmail.com>
2022-11-07 22:43:40 +0100

    rozawiazanie

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,36 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    ResourceManager() { res = new Resource(); };
+
+    ~ResourceManager() { delete res; };
+
+    double get() { return res->get(); };
+
+    ResourceManager(const ResourceManager& manager) { res = new Resource(*manager.res); };
+
+    ResourceManager& operator=(const ResourceManager& manager)
+    {
+        delete res;
+        res = new Resource(*manager.res);
+        return *this;
+    }
+
+    ResourceManager(ResourceManager&& manager) noexcept
+    {
+        res         = manager.res;
+        manager.res = nullptr;
+    }
+
+    ResourceManager& operator=(ResourceManager&& manager) noexcept
+    {
+        delete res;
+        res         = manager.res;
+        manager.res = nullptr;
+        return *this;
+    }
+
+private:
+    Resource* res = nullptr;
 };
